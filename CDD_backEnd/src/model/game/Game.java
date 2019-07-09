be708b1dd580612ead1f23b1a59b4ebe8cde7dc9
AhaLims <915673037@qq.com>
2019-07-09 09:58:26 +0800

    save

--- CDD_backEnd/src/model/game/Game.java
+++ CDD_backEnd/src/model/game/Game.java
@@ -49,15 +49,13 @@ public class Game{
 		return true;
 	}
 	
-	public void GameLogic() {
-		
-	}
+	
 	//出牌的函数
 	//传入当前的index.....
-	//这个函数由presentation来调用
+	//以及准备出的牌
 	//返回boolean值代表出的牌是不是合法的
-	//也许应该返回String 比较合适？ 如果是合法 则String为空
-	//不合法 String为相应的错误提示
+	//也许应该返回String 比较合适？ 如果是合法 则String为空     //不合法 String为相应的错误提示
+	
 	public boolean RoleDeliverCard(int index,List<Integer> list) {
 		boolean validation = false;
 		currentTurn = index;
@@ -70,6 +68,7 @@ public class Game{
 		//将转换成deleiverCardsGroup类型的
 		if(firstTurn == index && turnTime == 1)//第一轮的先手 
 		{
+			LatestCards = new deliveredCardsGroup();//先手 LatestCards 应该是新new的
 			//必须有方块三
 			if(currentCardsGroup.canFindCard(3, CardColor.Diamond) != -1)
 			{
@@ -79,6 +78,7 @@ public class Game{
 		}
 		if(IsFirstHand(index) == true)//非第一轮的先手
 		{
+			LatestCards = new deliveredCardsGroup();//先手 LatestCards 应该是新new的
 			if(currentCardsGroup.hasCards() == false)//TODO 这里是说明先手不出牌 是不合法的
 			{
 				validation = false;
@@ -113,6 +113,7 @@ public class Game{
 			{
 				System.out.print("【出牌】，并且为\n");
 				currentCardsGroup.showDetail();//展示牌组细节的数组
+				LatestCards = currentCardsGroup;//更新牌桌上的最后一组牌
 			}
 		
 		else
@@ -139,7 +140,7 @@ public class Game{
 		nextTurn[3] = 0;
 	}*/
 	//发牌
-	public void licensingCards() {
+	private void licensingCards() {
 		for(int i = 0;i < 52;i++) 
 		{
 			Card card = AllCards.getCardByIndex(i);
@@ -163,8 +164,11 @@ public class Game{
 			roles[ (currentTurn + i) % 4].setNumber(i);
 		}
 	}
-	public void turn() {
-		//TODO 这个turn应该是要一开始就调用的但是我不知道怎么以及什么时候调用比较好
+	//由presenter来调用这个函数 
+	//两个参数分别为:前端传来的牌的数组 以及presenter自己
+	public void turn(List<Integer> list) {
+		turnTime++;
+		deliveredCardsGroup currentCardsGroup = roles[0].selectCards(list);
 		
 	}
 	//虽然好像怪怪的....不应该调用...?
@@ -178,7 +182,7 @@ public class Game{
 		}
 		return -1;
 	}
-	public int getNextTurn() {
+	private int getNextTurn() {
 		return (currentTurn + 1) % 4;
 	}
 	
